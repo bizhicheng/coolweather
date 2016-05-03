75275d6117d5837af59cbab9f81b058dfc8510d3
Bruce <753033225@qq.com>
2016-05-03 22:34:22 +0800

    完成遍历省市县三级列表的功能。

--- src/com/example/coolweather/db/CoolWeatherOpenHelper.java
+++ src/com/example/coolweather/db/CoolWeatherOpenHelper.java
@@ -11,26 +11,26 @@ public class CoolWeatherOpenHelper extends SQLiteOpenHelper{
 	 * Province建表语句
 	 */
 	public static final String CREATE_PROVINCE = "create table Province ("
-			+ "id integer primary key autoincrement"
-			+ "province_name text"
+			+ "id integer primary key autoincrement,"
+			+ "province_name text,"
 			+ "province_code text)";
 			
 	/**
 	 * City建表语句
 	 */
 	public static final String CREATE_CITY = "create table City ("
-			+ "id integer primary key autoincrement"
-			+ "city_name text"
-			+ "city_code text"
+			+ "id integer primary key autoincrement,"
+			+ "city_name text,"
+			+ "city_code text,"
 			+ "province_id integer)";
 	
 	/**
 	 * County建表语句
 	 */
 	public static final String CREATE_COUNTY = "create table County ("
-			+ "id integer primary key autoincrement"
-			+ "county_name text"
-			+ "county_code text"
+			+ "id integer primary key autoincrement,"
+			+ "county_name text,"
+			+ "county_code text,"
 			+ "city_id integer)";
 			
 			
